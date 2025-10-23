5eae3f39f71e870b6fd51968cb85938ecfadfba3
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-10-22 23:24:23 -0400

    Play soccer: liftTowel method

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,14 @@
 public class Player {
+
+    private boolean hasID = false;
+    private boolean hasCleats = false;
+    private boolean hasTalkedToCoach = false;
+
+    public void look() {
+        return;
+    }
+
+    public String liftTowel() {
+        return "The towel has been lifted. You can pick your ID!";
+    }
 }
