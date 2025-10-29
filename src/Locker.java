43338a551a0847e71324b07587e56e21cc244e56
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-10-29 17:22:28 -0400

    Play soccer: Added locker methods

--- src/Locker.java
+++ src/Locker.java
@@ -2,23 +2,77 @@ public class Locker {
 //    Instance variables
     private boolean open;
     private boolean hasCleats;
-    private boolean unlocked;
+    private boolean isLocked;
 
     public Locker() {
         open = false;
         hasCleats = false;
-        unlocked = false;
+        isLocked = true;
     }
 
+//    this will show the player inside the locker room
     public void look() {
-        System.out.println("You see a locker door with a locker screen");
+        System.out.println("You see a locker with a combinational lockscreen.");
     }
 
+//    this will provide details about the locker
     public void examine() {
-        System.out.println("The locker is closed, and says 'Provide code to open'");
+        if (isLocked) {
+            System.out.println("A metal locker secured with a combinational lock. It appears to be locked.");
+        } else if (open) {
+            if (!hasCleats) {
+                System.out.println("The locker is open! Inside you see a pair of soccer cleats.");
+            }
+            else {
+                System.out.println("The locker is open and empty.");
+            }
+        }
+        else {
+            System.out.println("The locker is unlocked but still closed.");
+        }
     }
 
     public String unlock() {
         return "interact with combinational lock";
+    }
+
+//    this will help the player to open the locker to see cleats
+    public void openLocker() {
+        if (!isLocked && !open) {
+            open = true;
+            System.out.println("You open the locker door.");
+        } else if (isLocked) {
+            System.out.println("The locker is locked. You need to unlock it first.");
+        }
+        else {
+            System.out.println("The locker is already open.");
+        }
+    }
+
+//    this will help the player take the cleats from the locker
+    public void takeCleats() {
+        if (open && hasCleats) {
+            hasCleats = false;
+            System.out.println("You can take the cleats from the locker!");
+        }
+        else if (!open) {
+            System.out.println("You need to open the locker first.");
+        }
+        else {
+            System.out.println("There are no cleats left in the locker.");
+        }
+    }
+
+//    Getters methods
+    public boolean isOpen() {
+        return open;
+    }
+
+    public boolean hasCleats() {
+        return hasCleats;
+    }
+
+    public boolean isLocked() {
+        return isLocked;
     }
 }
