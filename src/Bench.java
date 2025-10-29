1b60f18df2a0a30a5675a0239b4d53ffec35c5d7
pinsoegustave <ntwalimudasubirag@gmail.com>
2025-10-29 16:19:51 -0400

    Play soccer: updated bench methods

--- src/Bench.java
+++ src/Bench.java
@@ -1,15 +1,62 @@
 public class Bench {
 
 //    Instance variables
-    private boolean hasID = false;
-    private boolean isLifted = false;
+    private boolean isIDTaken;
+    private boolean isLifted;
 
-    public boolean isLifted() {
-        return true;
+    public Bench() {
+        isLifted = false;
+        isIDTaken = false;
     }
 
-//    method to change the boolean hasID to true
-    public void getID() {
-        hasID = true;
+//    method that shows the bench
+    public void look() {
+        System.out.println("You see a bench with a towel on it.");
+    }
+
+//    method will provide details about the bench
+    public void examine() {
+        System.out.println("A bench with a towel placed above it.");
+        if (!isLifted) {
+            System.out.println("The towel looks like it might be hiding something...");
+        } else if (!isIDTaken) {
+            System.out.println("There is a card visible under the lifted towel");
+        }
+        else {
+            System.out.println("The bench is empty now.");
+        }
+
+    }
+
+    public void liftTowel() {
+        if (!isLifted) {
+            isLifted = true;
+            System.out.println("You have lifted the towel, and you see the ID card there!");
+        }
+        else {
+            System.out.println("The towel is already lifted.");
+        }
+    }
+
+//    method to take the ID from the towel
+    public void takeID() {
+        if (isLifted && !isIDTaken) {
+            isIDTaken = true;
+            System.out.println("You can take the ID, go talk to the coach!");
+        } else if (!isLifted) {
+            System.out.println("You need to lift the towel first!");
+        }
+        else {
+            System.out.println("You already have the ID.");
+        }
+    }
+
+//    Getters methods
+    public boolean isIDTaken() {
+        return isIDTaken;
+    }
+
+    public boolean isLifted() {
+        return isLifted;
     }
  }
